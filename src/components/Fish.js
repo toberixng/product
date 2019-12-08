78c2e1dedc2f21e62f2deb2e74cb9a0578edb3ff
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2019-12-08 02:02:04 +0100

    new component

---
+++ src/components/Fish.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Fish extends Component {
+    render() {
+        return (
+            <div className="singleFish">
+                 
+            </div>
+        )
+    }
+}
+
+export default Fish
