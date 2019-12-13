ed4a6fa1769ba3777c7892c035cb008483daf20a
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2019-12-13 19:28:42 +0100

    order update

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react'
 
 export class Order extends Component {
     render() {
-        const orderIds = Object.keys(this.props.order)
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((prevTotal, key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish && fish.status === 'available';
+            if(isAvailable){
+                return prevTotal + (count * fish.price)
+            }
+        }, 0);
         return (
             <div className="order-wrap">
                 <h2>Order</h2>
-                    {orderIds} 
+                    {orderIds}
+                    <div className="total">
+                        <strong>
+                            {total}
+                        </strong>
+                    </div>
             </div>
         )
     }
